5f0b5795c204191429185f399648aef82acae727
Janne Vanhala <janne.vanhala@gmail.com>
2012-02-22 22:09:09 +0200

    Initial import

---
+++ inflection.py
@@ -0,0 +1,278 @@
+import re
+
+_plurals = []
+_singulars = []
+_uncountables = set()
+
+
+def plural(rule, replacement, flags=re.IGNORECASE):
+    if rule in _uncountables:
+        _uncountables.remove(rule)
+    if replacement in _uncountables:
+        _uncountables.remove(replacement)
+    _plurals.insert(0, (re.compile(rule, flags), replacement))
+
+
+def singular(rule, replacement, flags=re.IGNORECASE):
+    """
+    Specifies a new singularization rule and its replacement. The rule can
+    either be a string or a regular expression. The replacement should always
+    be a string that may include references to the matched data from the rule.
+    """
+    if rule in _uncountables:
+        _uncountables.remove(rule)
+    if replacement in _uncountables:
+        _uncountables.remove(replacement)
+    _singulars.insert(0, (re.compile(rule, flags), replacement))
+
+
+def irregular(s, p):
+    """
+    Specifies a new irregular that applies to both pluralization and
+    singularization at the same time. This can only be used for strings, not
+    regular expressions. You simply pass the irregular in singular and plural
+    form.
+
+    Examples::
+
+        >>> irregular('octopus', 'octopi')
+        >>> irregular('person', 'people')
+
+    :param s: irregular in singular form
+    :param p: irregular in plural form
+    """
+    if s in _uncountables:
+        _uncountables.remove(s)
+    if p in _uncountables:
+        _uncountables.remove(p)
+    if s[0].upper() == p[0].upper():
+        plural(r"({0}){1}$".format(s[0], s[1:]), r'\1' + p[1:])
+        plural(r"({0}){1}$".format(p[0], p[1:]), r'\1' + p[1:])
+        singular(r"({0}){1}$".format(p[0], p[1:]), r'\1' + s[1:])
+    else:
+        plural(r"{0}(?i){1}$".format(s[0].upper(), s[1:]),
+            p[0].upper() + p[1:], flags=0)
+        plural(r"{0}(?i){1}$".format(s[0].lower(), s[1:]),
+            p[0].lower() + p[1:], flags=0)
+        plural(r"{0}(?i){1}$".format(p[0].upper(), p[1:]),
+            p[0].upper() + p[1:], flags=0)
+        plural(r"{0}(?i){1}$".format(p[0].lower(), p[1:]),
+            p[0].lower() + p[1:], flags=0)
+        singular(r"{0}(?i){1}$".format(p[0].upper(), p[1:]),
+            s[0].upper() + s[1:], flags=0)
+        singular(r"{0}(?i){1}$".format(p[0].lower(), p[1:]),
+            s[0].lower() + s[1:], flags=0)
+
+
+def uncountable(*words):
+    """
+    Add uncountable words that shouldn't be attempted inflected.
+
+    Examples::
+
+        >>> uncountable("money")
+        >>> uncountable("money", "information")
+    """
+    for word in words:
+        _uncountables.add(word)
+
+
+def camelize(string, uppercase_first_letter=True):
+    """
+    Convert strings to CamelCase.
+
+    Examples::
+
+        >>> camelize("device_type")
+        "DeviceType"
+        >>> camelize("device_type", False)
+        "deviceType"
+
+    `camelize` can be though as a inverse of :func:`underscore`, although
+    there are some cases where that does not hold::
+
+        >>> camelize(underscore("IOError"))
+        "IoError"
+
+    :param uppercase_first_letter: if set to `True` `camelize` converts
+        strings to UpperCamelCase. If set to `False` `camelize` products
+        lowerCamelCase. Defaults to `True`.
+    """
+    if uppercase_first_letter:
+        return re.sub(r"(?:^|_)(.)", lambda m: m.group(1).upper(), string)
+    else:
+        return string[0].lower() + camelize(string)[1:]
+
+
+def classify():
+    pass
+
+
+def dasherize(word):
+    """Replace underscores with dashes in the string.
+
+    Example::
+
+        >>> dasherize("puni_puni")
+        "puni-puni"
+    """
+    return word.replace('_', '-')
+
+
+def foreign_key():
+    pass
+
+
+def humanize():
+    pass
+
+
+def ordinalize():
+    pass
+
+
+def parameterize():
+    pass
+
+
+def pluralize(word):
+    """
+    Return the plural form of a word.
+
+    Examples::
+
+        >>> pluralize("post")
+        "posts"
+        >>> pluralize("octopus")
+        "octopi"
+        >>> pluralize("sheep")
+        "sheep"
+        >>> pluralize("CamelOctopus")
+        "CamelOctopi"
+
+    """
+    if not word or word.lower() in _uncountables:
+        return word
+    else:
+        for rule, replacement in _plurals:
+            if rule.search(word):
+                return rule.sub(replacement, word)
+        return word
+
+
+def singularize(word):
+    """
+    Return the singular form of a word, the reverse of :func:`pluralize`.
+
+    Examples::
+
+        >>> singularize("posts")
+        "post"
+        >>> singularize("octopi")
+        "octopus"
+        >>> singularize("sheep")
+        "sheep"
+        >>> singularize("word")
+        "word"
+        >>> singularize("CamelOctopi")
+        "CamelOctopus"
+    """
+    for inflection in _uncountables:
+        if re.search(r'\b({0})\Z'.format(inflection), word, flags=re.IGNORECASE):
+            return word
+
+    for rule, replacement in _singulars:
+        if rule.search(word):
+            return rule.sub(replacement, word)
+    return word
+
+
+def tableize():
+    pass
+
+
+def titleize():
+    pass
+
+
+def transliterate():
+    pass
+
+
+def underscore(word):
+    word = re.sub(r"([A-Z]+)([A-Z][a-z])", r'\1_\2', word)
+    word = re.sub(r"([a-z\d])([A-Z])", r'\1_\2', word)
+    word = word.replace("-", "_")
+    return word.lower()
+
+
+plural(r"$", 's')
+plural(r"s$", 's')
+plural(r"^(ax|test)is$", r'\1es')
+plural(r"(octop|vir)us$", r'\1i')
+plural(r"(octop|vir)i$", r'\1i')
+plural(r"(alias|status)$", r'\1es')
+plural(r"(bu)s$", r'\1ses')
+plural(r"(buffal|tomat)o$", r'\1oes')
+plural(r"([ti])um$", r'\1a')
+plural(r"([ti])a$", r'\1a')
+plural(r"sis$", 'ses')
+plural(r"([^f])fe$", r'\1ves')
+plural(r"([lr])f$", r'\1ves')
+plural(r"(hive)$", r'\1s')
+plural(r"([^aeiouy]|qu)y$", r'\1ies')
+plural(r"(x|ch|ss|sh)$", r'\1es')
+plural(r"(matr|vert|ind)(?:ix|ex)$", r'\1ices')
+plural(r"(m|l)ouse$", r'\1ice')
+plural(r"(m|l)ice$", r'\1ice')
+plural(r"^(ox)$", r'\1en')
+plural(r"^(oxen)$", r'\1')
+plural(r"(quiz)$", r'\1zes')
+
+singular(r"s$", '')
+singular(r"(ss)$", r'\1')
+singular(r"(n)ews$", r'\1ews')
+singular(r"([ti])a$", r'\1um')
+singular(r"((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$", r'\1\2sis')
+singular(r"(^analy)(sis|ses)$", r'\1sis')
+singular(r"([^f])ves$", r'\1fe')
+singular(r"(hive)s$", r'\1')
+singular(r"(tive)s$", r'\1')
+singular(r"([lr])ves$", r'\1f')
+singular(r"([^aeiouy]|qu)ies$", r'\1y')
+singular(r"(s)eries$", r'\1eries')
+singular(r"(m)ovies$", r'\1ovie')
+singular(r"(x|ch|ss|sh)es$", r'\1')
+singular(r"(m|l)ice$", r'\1ouse')
+singular(r"(bus)(es)?$", r'\1')
+singular(r"(o)es$", r'\1')
+singular(r"(shoe)s$", r'\1')
+singular(r"(cris|test)(is|es)$", r'\1is')
+singular(r"^(a)x[ie]s$", r'\1xis')
+singular(r"(octop|vir)(us|i)$", r'\1us')
+singular(r"(alias|status)(es)?$", r'\1')
+singular(r"^(ox)en", r'\1')
+singular(r"(vert|ind)ices$", r'\1ex')
+singular(r"(matr)ices$", r'\1ix')
+singular(r"(quiz)zes$", r'\1')
+singular(r"(database)s$", r'\1')
+
+irregular('person', 'people')
+irregular('man', 'men')
+irregular('child', 'children')
+irregular('sex', 'sexes')
+irregular('move', 'moves')
+irregular('cow', 'kine')
+irregular('zombie', 'zombies')
+
+uncountable(
+    'equipment',
+    'fish',
+    'information',
+    'jeans'
+    'money',
+    'rice',
+    'series',
+    'sheep',
+    'species',
+)
---
+++ test_inflection.py
@@ -0,0 +1,205 @@
+import inflection
+import pytest
+
+
+SINGULAR_TO_PLURAL = (
+    ("search", "searches"),
+    ("switch", "switches"),
+    ("fix", "fixes"),
+    ("box", "boxes"),
+    ("process", "processes"),
+    ("address", "addresses"),
+    ("case", "cases"),
+    ("stack", "stacks"),
+    ("wish", "wishes"),
+    ("fish", "fish"),
+    ("jeans", "jeans"),
+    ("funky jeans", "funky jeans"),
+
+    ("category", "categories"),
+    ("query", "queries"),
+    ("ability", "abilities"),
+    ("agency", "agencies"),
+    ("movie", "movies"),
+
+    ("archive", "archives"),
+
+    ("index", "indices"),
+
+    ("wife", "wives"),
+    ("safe", "saves"),
+    ("half", "halves"),
+
+    ("move", "moves"),
+
+    ("salesperson", "salespeople"),
+    ("person", "people"),
+
+    ("spokesman", "spokesmen"),
+    ("man", "men"),
+    ("woman", "women"),
+
+    ("basis", "bases"),
+    ("diagnosis", "diagnoses"),
+    ("diagnosis_a", "diagnosis_as"),
+
+    ("datum", "data"),
+    ("medium", "media"),
+    ("stadium", "stadia"),
+    ("analysis", "analyses"),
+
+    ("node_child", "node_children"),
+    ("child", "children"),
+
+    ("experience", "experiences"),
+    ("day", "days"),
+
+    ("comment", "comments"),
+    ("foobar", "foobars"),
+    ("newsletter", "newsletters"),
+
+    ("old_news", "old_news"),
+    ("news", "news"),
+
+    ("series", "series"),
+    ("species", "species"),
+
+    ("quiz", "quizzes"),
+
+    ("perspective", "perspectives"),
+
+    ("ox", "oxen"),
+    ("photo", "photos"),
+    ("buffalo", "buffaloes"),
+    ("tomato", "tomatoes"),
+    ("dwarf", "dwarves"),
+    ("elf", "elves"),
+    ("information", "information"),
+    ("equipment", "equipment"),
+    ("bus", "buses"),
+    ("status", "statuses"),
+    ("status_code", "status_codes"),
+    ("mouse", "mice"),
+
+    ("louse", "lice"),
+    ("house", "houses"),
+    ("octopus", "octopi"),
+    ("virus", "viri"),
+    ("alias", "aliases"),
+    ("portfolio", "portfolios"),
+
+    ("vertex", "vertices"),
+    ("matrix", "matrices"),
+    ("matrix_fu", "matrix_fus"),
+
+    ("axis", "axes"),
+    ("testis", "testes"),
+    ("crisis", "crises"),
+
+    ("rice", "rice"),
+    ("shoe", "shoes"),
+
+    ("horse", "horses"),
+    ("prize", "prizes"),
+    ("edge", "edges"),
+
+    ("cow", "kine"),
+    ("database", "databases"),
+)
+
+CAMEL_TO_UNDERSCORE = (
+    ("Product",               "product"),
+    ("SpecialGuest",          "special_guest"),
+    ("ApplicationController", "application_controller"),
+    ("Area51Controller",      "area51_controller"),
+)
+
+CAMEL_TO_UNDERSCORE_WITHOUT_REVERSE = (
+    ("HTMLTidy",              "html_tidy"),
+    ("HTMLTidyGenerator",     "html_tidy_generator"),
+    ("FreeBSD",               "free_bsd"),
+    ("HTML",                  "html"),
+)
+
+UNDERSCORES_TO_DASHES = (
+    ("street",                "street"),
+    ("street_address",        "street-address"),
+    ("person_street_address", "person-street-address"),
+)
+
+
+def test_pluralize_plurals():
+    assert "plurals" == inflection.pluralize("plurals")
+    assert "Plurals" == inflection.pluralize("Plurals")
+
+
+def test_pluralize_empty_string():
+    assert "" == inflection.pluralize("")
+
+
+@pytest.mark.parametrize(("word", ),
+    [(word,) for word in inflection._uncountables])
+def test_uncountability(word):
+    assert word == inflection.singularize(word)
+    assert word == inflection.pluralize(word)
+    assert inflection.pluralize(word) == inflection.singularize(word)
+
+
+def test_uncountable_word_is_not_greedy():
+    uncountable_word = "ors"
+    countable_word = "sponsor"
+
+    inflection._uncountables.add(uncountable_word)
+    try:
+        assert uncountable_word == inflection.singularize(uncountable_word)
+        assert uncountable_word == inflection.pluralize(uncountable_word)
+        assert inflection.pluralize(uncountable_word) == inflection.singularize(uncountable_word)
+
+        assert "sponsor" == inflection.singularize(countable_word)
+        assert "sponsors" == inflection.pluralize(countable_word)
+        assert "sponsor" == inflection.singularize(inflection.pluralize(countable_word))
+    finally:
+        inflection._uncountables.remove(uncountable_word)
+
+
+@pytest.mark.parametrize(("singular", "plural"), SINGULAR_TO_PLURAL)
+def test_pluralize_singular(singular, plural):
+    assert plural == inflection.pluralize(singular)
+    assert plural.capitalize() == inflection.pluralize(singular.capitalize())
+
+
+@pytest.mark.parametrize(("singular", "plural"), SINGULAR_TO_PLURAL)
+def test_singularize_plural(singular, plural):
+    assert singular == inflection.singularize(plural)
+    assert singular.capitalize() == inflection.singularize(plural.capitalize())
+
+
+@pytest.mark.parametrize(("singular", "plural"), SINGULAR_TO_PLURAL)
+def test_pluralize_plural(singular, plural):
+    assert plural == inflection.pluralize(plural)
+    assert plural.capitalize() == inflection.pluralize(plural.capitalize())
+
+
+@pytest.mark.parametrize(("camel", "underscore"), CAMEL_TO_UNDERSCORE)
+def test_camelize(camel, underscore):
+    assert camel == inflection.camelize(underscore)
+
+
+def test_camelize_with_lower_downcases_the_first_letter():
+    assert 'capital' == inflection.camelize('Capital', False)
+
+
+def test_camelize_with_underscores():
+    assert "CamelCase" == inflection.camelize('Camel_Case')
+
+
+@pytest.mark.parametrize(("camel", "underscore"),
+    CAMEL_TO_UNDERSCORE + CAMEL_TO_UNDERSCORE_WITHOUT_REVERSE
+)
+def test_underscore(camel, underscore):
+    assert underscore == inflection.underscore(camel)
+
+
+@pytest.mark.parametrize(("input", "expected"), UNDERSCORES_TO_DASHES)
+def test_dasherize(input, expected):
+    assert inflection.dasherize(input) == expected
