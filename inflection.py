1d461c7c16e195b732437a5e516a8d6ee2ad9660
Janne Vanhala <janne.vanhala@gmail.com>
2012-02-24 00:36:05 +0200

    docstring

--- inflection.py
+++ inflection.py
@@ -316,6 +316,21 @@ def transliterate():
 
 
 def underscore(word):
+    """
+    Makes an underscored, lowercase form from the expression in the string.
+
+    Example::
+
+        >>> underscore("DeviceType")
+        "device_type"
+
+    As a rule of thumb you can think of :func:`underscore` as the inverse of
+    :func:`camelize`, though there are cases where that does not hold::
+
+        >>> camelize(underscore("IOError"))
+        "IoError"
+
+    """
     word = re.sub(r"([A-Z]+)([A-Z][a-z])", r'\1_\2', word)
     word = re.sub(r"([a-z\d])([A-Z])", r'\1_\2', word)
     word = word.replace("-", "_")
