6933499d5881a62f1563186aed45d2cbe26f1912
Janne Vanhala <janne.vanhala@gmail.com>
2012-02-24 00:29:20 +0200

    added ordinalize

--- inflection.py
+++ inflection.py
@@ -197,8 +197,57 @@ def humanize(word):
     return word
 
 
-def ordinalize():
-    pass
+def ordinal(number):
+    """
+    Returns the suffix that should be added to a number to denote the position
+    in an ordered sequence such as 1st, 2nd, 3rd, 4th.
+
+    Examples::
+        >>> ordinal(1)
+        "st"
+        >>> ordinal(2)
+        "nd"
+        >>> ordinal(1002)
+        "nd"
+        >>> ordinal(1003)
+        "rd"
+        >>> ordinal(-11)
+        "th"
+        >>> ordinal(-1021)
+        "st"
+    """
+    number = abs(int(number))
+    if number % 100 in (11, 12, 13):
+        return "th"
+    else:
+        return {
+            1: "st",
+            2: "nd",
+            3: "rd",
+        }.get(number % 10, "th")
+
+
+def ordinalize(number):
+    """
+    Turns a number into an ordinal string used to denote the position in an
+    ordered sequence such as 1st, 2nd, 3rd, 4th.
+
+    Examples::
+
+        >>> ordinalize(1)
+        "1st"
+        >>> ordinalize(2)
+        "2nd"
+        >>> ordinalize(1002)
+        "1002nd"
+        >>> ordinalize(1003)
+        "1003rd"
+        >>> ordinalize(-11)
+        "-11th"
+        >>> ordinalize(-1021)
+        "-1021st"
+    """
+    return "%s%s" % (number, ordinal(number))
 
 
 def parameterize():
--- test_inflection.py
+++ test_inflection.py
@@ -127,6 +127,70 @@ UNDERSCORE_TO_HUMAN = (
     ("underground",           "Underground"),
 )
 
+ORDINAL_NUMBERS = (
+    ("-1", "-1st"),
+    ("-2", "-2nd"),
+    ("-3", "-3rd"),
+    ("-4", "-4th"),
+    ("-5", "-5th"),
+    ("-6", "-6th"),
+    ("-7", "-7th"),
+    ("-8", "-8th"),
+    ("-9", "-9th"),
+    ("-10", "-10th"),
+    ("-11", "-11th"),
+    ("-12", "-12th"),
+    ("-13", "-13th"),
+    ("-14", "-14th"),
+    ("-20", "-20th"),
+    ("-21", "-21st"),
+    ("-22", "-22nd"),
+    ("-23", "-23rd"),
+    ("-24", "-24th"),
+    ("-100", "-100th"),
+    ("-101", "-101st"),
+    ("-102", "-102nd"),
+    ("-103", "-103rd"),
+    ("-104", "-104th"),
+    ("-110", "-110th"),
+    ("-111", "-111th"),
+    ("-112", "-112th"),
+    ("-113", "-113th"),
+    ("-1000", "-1000th"),
+    ("-1001", "-1001st"),
+    ("0", "0th"),
+    ("1", "1st"),
+    ("2", "2nd"),
+    ("3", "3rd"),
+    ("4", "4th"),
+    ("5", "5th"),
+    ("6", "6th"),
+    ("7", "7th"),
+    ("8", "8th"),
+    ("9", "9th"),
+    ("10", "10th"),
+    ("11", "11th"),
+    ("12", "12th"),
+    ("13", "13th"),
+    ("14", "14th"),
+    ("20", "20th"),
+    ("21", "21st"),
+    ("22", "22nd"),
+    ("23", "23rd"),
+    ("24", "24th"),
+    ("100", "100th"),
+    ("101", "101st"),
+    ("102", "102nd"),
+    ("103", "103rd"),
+    ("104", "104th"),
+    ("110", "110th"),
+    ("111", "111th"),
+    ("112", "112th"),
+    ("113", "113th"),
+    ("1000", "1000th"),
+    ("1001", "1001st"),
+)
+
 UNDERSCORES_TO_DASHES = (
     ("street",                "street"),
     ("street_address",        "street-address"),
@@ -206,11 +270,19 @@ def test_underscore(camel, underscore):
     assert underscore == inflection.underscore(camel)
 
 
-@pytest.mark.parametrize(("underscore", "human"),
-    UNDERSCORE_TO_HUMAN
-)
+@pytest.mark.parametrize(("underscore", "human"), UNDERSCORE_TO_HUMAN)
 def test_humanize(underscore, human):
     assert human == inflection.humanize(underscore)
+
+
+@pytest.mark.parametrize(("number", "ordinalized"), ORDINAL_NUMBERS)
+def test_ordinal(number, ordinalized):
+    assert ordinalized == number + inflection.ordinal(number)
+
+
+@pytest.mark.parametrize(("number", "ordinalized"), ORDINAL_NUMBERS)
+def test_ordinalize(number, ordinalized):
+    assert ordinalized == inflection.ordinalize(number)
 
 
 @pytest.mark.parametrize(("input", "expected"), UNDERSCORES_TO_DASHES)
