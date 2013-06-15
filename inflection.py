405d5540ea37d361060226794e7f8a531ea8d4cb
Janne Vanhala <janne.vanhala@gmail.com>
2013-06-15 17:27:06 +0300

    Updated copyrights

--- inflection.py
+++ inflection.py
@@ -5,8 +5,7 @@
 
     A port of Ruby on Rails' inflector to Python.
 
-    :copyright: (c) 2012 by Janne Vanhala
-    :copyright: (c) 2005-2012 by David Heinemeier Hansson
+    :copyright: (c) 2013 by Janne Vanhala
 
     :license: MIT, see LICENSE for more details.
 """
