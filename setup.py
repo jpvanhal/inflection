ae51e3cd4dfb0c9580aefc1e7fc6e916d9214a54
Janne Vanhala <janne.vanhala@gmail.com>
2013-06-15 17:38:13 +0300

    Updated classifiers.

--- setup.py
+++ setup.py
@@ -41,8 +41,9 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ),
 )
