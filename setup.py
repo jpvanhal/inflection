f0ef09e37909f0be8036989a44b7d8ff961ae3a4
Hugo <hugovk@users.noreply.github.com>
2018-01-19 22:44:07 +0200

    Add Python 3.6, drop EOL 2.6 and 3.3

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     license='MIT',
     py_modules=['inflection'],
     zip_safe=False,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -26,11 +27,12 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
