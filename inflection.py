3a049f152d0f80ad957eaec43ef076a777ede5e9
Janne Vanhala <janne.vanhala@gmail.com>
2020-04-06 22:16:43 +0300

    Bump copyright year

--- inflection.py
+++ inflection.py
@@ -5,7 +5,7 @@
 
     A port of Ruby on Rails' inflector to Python.
 
-    :copyright: (c) 2012-2015 by Janne Vanhala
+    :copyright: (c) 2012-2020 by Janne Vanhala
 
     :license: MIT, see LICENSE for more details.
 """
